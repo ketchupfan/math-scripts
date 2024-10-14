d8e020668f3b45681063bf88285cf8463092ce58
ketchupfan <131740487+ketchupfan@users.noreply.github.com>
2024-10-14 00:30:44 -0500

    Add files via upload

---
+++ multmodgroup.py
@@ -0,0 +1,25 @@
+from math import gcd
+multmodgroup = []
+
+target = int(input("Which multiplication integer modulo group? "))
+print()
+
+for x in range(target):
+    if gcd(x, target) == 1:
+        multmodgroup.append(x)
+
+print(multmodgroup)
+print()
+
+for x in multmodgroup:
+    factor = x
+    iter = 0
+    while True:
+        print(x % target, end =" ")
+        iter += 1
+        if (x % target == 1 or iter > 100):
+            break
+        
+        x = x * factor
+        
+    print()
