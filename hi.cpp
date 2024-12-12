150358fe927b73873362b85eba38202a2cef2200
ketchupfan <kevinfan2026@u.northwestern.edu>
2024-12-12 17:56:19 -0600

    asdf

---
+++ hi.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string userinput;
+
+    cout << "enter something: ";
+    cin >> userinput;
+    cout << "you entered " << userinput << endl;
+
+    return 0;
+}
